c8997e1d1ba8a3e204263b8202d8c5c2fae67049
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2022-11-10 23:31:54 -0300

    add pwa and cache

---
+++ sw.js
@@ -0,0 +1,36 @@
+const nombreCache = 'apv-v1';
+
+const archivos = [
+    '/',
+    'index.html',
+    './css/bootstrap.css',
+    './css/styles.css',
+    './js/app.js',
+    './js/apv.js'
+];
+
+
+// Cuando se instala el service Worker
+self.addEventListener('install', e => {
+    console.log('Instalado el Service Worker');
+
+    e.waitUntil(
+        caches.open(nombreCache)
+        .then( cache =>{
+            console.log('Cacheando');
+            cache.addAll(archivos)
+        })
+    )
+});
+
+// Activar el Service Worker 
+self.addEventListener('activate', e => {
+    console.log('Service Worker Activado');
+
+    console.log(e);
+})
+
+// Evento fetch para descargar archivos estaticos
+self.addEventListener('fetch', e => {
+    console.log('Fetch...', e);
+} );
